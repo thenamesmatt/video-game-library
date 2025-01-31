7cd867290b86217c074c97d541b51dc951448687
Matthew OBrien <matthew.obrien5485@gmail.com>
2025-01-30 19:57:15 -0500

    Final working system of Building Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [isShowing, setIsShowing] = useState(false);
+
+  function pressed() {
+    setIsShowing(!isShowing)
+  }
+
   return (
     <div>
-      <ListGroup />
+      {isShowing && <Alert onClose={pressed}>Hello</Alert>}
+      <Button onClick={pressed} color="primary">
+        Button
+      </Button>
     </div>
   );
 }
---
+++ src/components/Alert.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, useState } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClose: () => void
+}
+
+function Alert({ children, onClose }: Props) {
+
+  return (
+    <div className={"alert alert-warning alert-dismissible fade show"} role="alert">
+      <strong>Holy guacamole!</strong> You should check in on some of those
+      fields below.
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
+}
+
+export default Alert;
